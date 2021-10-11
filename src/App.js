7129d3d14342f56c92da74d129167c611cbc273b
Yornan0325 <jhornan.coningenieria@gmail.com>
2021-10-10 21:17:57 -0500

    cinfig App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
- import './App.css';
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import PrivateRoute from "./Access/RestrictAccess/PrivateRoute";
+import SignIn from "./Access/SignIn/SignIn";
+import SignUp from "./Access/SignOut/SignOut";
+import { ProvidersApp } from "./Context/UseContext";
+
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    // <div className="App">
+      <ProvidersApp>
+        <Router>
+          <PrivateRoute exact path="/" component={Home} />
+          <Paths/>
+          <Route exact path="/login" component={SignIn} />
+          <Route exact path="/signup" component={SignUp} />
+        </Router>
+      </ProvidersApp>
+    // </div>
   );
 }
 
